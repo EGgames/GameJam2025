ac1195eeffdaa4610d3de0120d17dd6cc8580dfa
Acrux <acruxstudios.dev@gmail.com>
2025-01-25 17:00:56 -0300

    Proyectile enemies can now aim at a target

--- Assets/scripts/ProyectileEnemy.cs
+++ Assets/scripts/ProyectileEnemy.cs
@@ -6,6 +6,7 @@ public class ProyectileEnemy : MonoBehaviour
     // The interval between this enemy's shots (in seconds)
     public float shootIntervalSecs;
     public GameObject proyectilePrefab;
+    public GameObject target;
 
     // How much time the enemy has to wait to shoot again (in seconds)
     private float shootCooldownSecs = 0;
@@ -18,6 +19,7 @@ public class ProyectileEnemy : MonoBehaviour
     private void Update()
     {
         TickShotInterval();
+        TickAim();
     }
 
     private void TickShotInterval()
@@ -28,6 +30,15 @@ public class ProyectileEnemy : MonoBehaviour
             Shoot();
             shootCooldownSecs = shootIntervalSecs;
         }
+    }
+
+    private void TickAim()
+    {
+        Vector2 targetPosition = target.transform.position;
+        Vector2 direction = targetPosition - (Vector2)transform.position;
+        //-90 is used to use transform.up as the front of the enemy
+        float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) -90;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     public void Shoot()
